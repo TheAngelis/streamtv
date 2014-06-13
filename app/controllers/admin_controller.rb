19641bb8d6bb38dcc49185e6ae450c47d302bb0f
Rodrigo de Angelis <de4ngelis@Angelis.local>
2014-06-13 16:54:43 -0300

    version 01

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,27 @@
+class AdminController < ApplicationController
+
+  before_filter :authenticate_user!
+before_filter :is_admin?
+
+def is_admin?
+  if current_user.admin?
+    true
+  else
+    redirect_to :controller=>'dashboard', :action => 'index'
+  end
+end
+
+
+  def index
+
+  end
+
+#  def show
+
+#  if current_user.admin?
+   #     redirect_to :controller=>'users', :action => 'show'
+   # end
+  #end
+
+
+end
