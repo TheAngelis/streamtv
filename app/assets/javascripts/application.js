ecb900ffce4de80463249c66e7694b37275976ba
Rodrigo de Angelis <de4ngelis@angelis.home>
2014-07-30 04:40:37 -0300

    atualizacao de layot, countdown, e correcoes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,3 +38,46 @@ function adjustIframes()
   });
 }
 $(window).on('resize load',adjustIframes);
+
+
+
+
+
+
+
+// declare weddingdate and data variables
+var weddingDate = new Date("Jul 31, 2014 15:00:00"),
+    months, days, minutes, seconds,
+    //declare Dom position
+    countdown = document.getElementsByClassName("integer");
+console.log(countdown);
+
+// Set padding zero's to 1 number integers
+function padZero(number){
+  return String("0" + number).slice(-2);
+}
+
+setInterval(function() {
+  var now = Date.now();
+  var trackTime = (weddingDate - now)/1000;
+
+  //convert miliseconds to readable date
+  years = padZero(parseInt(trackTime / 31556926));
+  trackTime = trackTime % 31556926;
+  months = padZero(parseInt(trackTime / 2629743.83));
+  trackTime = trackTime % 2629743.83;
+  days = padZero(parseInt(trackTime / 86400));
+  trackTime = trackTime % 86400;
+  hours = padZero(parseInt(trackTime / 3600));
+  trackTime = trackTime % 3600;
+  minutes = padZero(parseInt(trackTime / 60));
+  trackTime = trackTime % 60;
+  seconds = padZero(parseInt(trackTime));
+  //write date
+
+  countdown[0].innerHTML = months;
+  countdown[1].innerHTML = days;
+  countdown[2].innerHTML = hours;
+  countdown[3].innerHTML = minutes;
+  countdown[4].innerHTML = seconds;
+}, 1000)
